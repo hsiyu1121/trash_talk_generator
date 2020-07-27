0f4b236c70d203661ae58717ee082b337ea90650
hsiyu1121 <bluefish1121@gmail.com>
2020-07-27 15:19:25 +0800

    Update app.js, generate.js, index.handlebars

--- app.js
+++ app.js
@@ -4,21 +4,34 @@ const port = 3000
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const generateTalk = require('./generate_talk')
+const Handlebars = require('handlebars')
 
 app.engine('handlebars', exphbs({defaultLayout:'main'}))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({extended:true}))
+
+Handlebars.registerHelper('if_job', function(job, equalJob, options){
+  if(job === equalJob){
+    return options.fn(this)
+  }else{
+    return options.inverse(this)
+  }
+})
+
 
 app.get('/', (req, res)=>{
   res.render('index')
 })
 
 app.post('/', (req, res)=>{
-  const option = req.body
-  const trashTalk = generateTalk(option) 
-  const person = option.job
-  res.render('index', {trashTalk:trashTalk, person:person})
+  const options = req.body.job
+  const trashTalk = generateTalk(options) 
+  const job = req.body.job
+  // console.log(options)
+  // console.log( generateTalk(options) )
+  res.render('index', {trashTalk:trashTalk, job:job})
 })
+
 
 app.listen(port, ()=>{
   console.log(`Express is running on localhost:${port}`)
--- generate_talk.js
+++ generate_talk.js
@@ -1,31 +1,39 @@
-function generateTalk(option) {
+function randomTask(arr){
+  const randomIndex = Math.floor(Math.random() * arr.length)
+  return arr[randomIndex]
+}
+
+function generateTalk(options) {
+ 
   const task = {
   engineer: ['加個按鈕','加新功能','切個版', '改一點 code'],
   designer: ['畫一張圖', '改個 logo','順便幫忙設計一下','隨便換個設計'],
   entrepreneur: ['週末加班', '要能賺錢','想個 business model','找 VC 募錢']
-}
-
-const phrase = ['很簡單','很容易','很快','很正常']
-
-  let talkTo = ''
-
-  if (!option.job ){
-    talkTo = '請選擇一個職業！！'
-  }else if(option.job === 'engineer'){
-    talkTo += '身為一個工程師，' + task.engineer[randomWord()] + '， ' + phrase[randomWord()]
-  }else if(option.job === 'designer'){
-    talkTo += '身為一個設計師，' + task.designer[randomWord()] + '， ' + phrase[randomWord()]
-  }else if(option.job === 'entrepreneur'){
-    talkTo += '身為一個創業家，' + task.entrepreneur[randomWord()] + '， ' + phrase[randomWord()]
   }
 
-  return talkTo
-}
+  const phrase = ['很簡單','很容易','很快','很正常']
 
-  function randomWord(){
-    return  Math.floor(Math.random() * 4 )
+  const randomPhrase = randomTask(phrase) 
+  let jobName = ''
+  let talkTo = []
+  let trashTalk = ''
+
+  if(options === undefined){
+    return '請選擇一個職業！！'
+  }else if(options === 'engineer'){
+    talkTo = randomTask(task.engineer) 
+    jobName = '工程師'
+  }else if(options === 'designer'){
+    talkTo = randomTask(task.designer) 
+    jobName = '設計師'
+  }else if(options === 'entrepreneur'){
+    talkTo = randomTask(task.entrepreneur) 
+    jobName = '創業家'
   }
 
+  trashTalk = `身為一個${jobName}, ${talkTo}, ${randomPhrase}`
+  return trashTalk
+}
 
 module.exports = generateTalk
 
